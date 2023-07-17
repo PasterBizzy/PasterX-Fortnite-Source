22dddb0c162f69f9e54709a077f7b2dee5c0e7ad
PasterBizzy <122704704+PasterBizzy@users.noreply.github.com>
2023-07-17 20:40:47 +0100

    Update offsets.h

--- reverse/offsets.h
+++ reverse/offsets.h
@@ -1,13 +1,14 @@
 #pragma once
-#pragma once
 #include <Windows.h>
-#define OFFSET_UWORLD 0x102f69d8 //
-//
+
+#define GWorld 0xEA84A78
+#define VIEW_POINT 0xE9AD420
+
 
 namespace OFFSETS
-
-
 {
+    uintptr_t ViewPoint = 0xE9AD420;
+    uintptr_t UWorld = 0xEA84A78;
     uintptr_t Gameinstance = 0x1b8;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
@@ -22,13 +23,15 @@ namespace OFFSETS
     uintptr_t AActor = 0x98;
     uintptr_t CurrentActor = 0x8;
     uintptr_t Mesh = 0x310;
-    uintptr_t Revivefromdbnotime = 0x4530;
-    uintptr_t TeamId = 0x10B0;
-    uintptr_t ActorTeamId = 0x10B0;
+    uintptr_t Revivefromdbnotime = 0x4698;
+    uintptr_t TeamId = 0x10C8;
+    uintptr_t ActorTeamId = 0x10C8;
+    uintptr_t IsDBNO = 0x872;
 
     uintptr_t LocalActorPos = 0x128;
     uintptr_t ComponetToWorld = 0x240;
-    uintptr_t BoneArray = 0x5f0;
+    uintptr_t BoneArray = 0x600;
+    uintptr_t Bonecache = 0x640;
     uintptr_t Velocity = 0xb8;
     uintptr_t PawnPrivate = 0x300;
     uintptr_t PlayerArray = 0x2a0;
@@ -37,5 +40,5 @@ namespace OFFSETS
     uintptr_t entity_actor = 0x310;//havent got
     uintptr_t bIsReloadingWeapon = 0x331;
     uintptr_t GlobalAnimRateScale = 0xa58;
-    uintptr_t CurrentWeapon = 0x908;
+    uintptr_t CurrentWeapon = 0x948;
 }
